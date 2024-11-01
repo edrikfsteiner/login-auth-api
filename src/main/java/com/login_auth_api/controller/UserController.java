ca6aa80e998bc677e7ab5c55bda186dbc2ec2571
edrikfsteiner <edrikfsteiner@gmail.com>
2024-11-01 20:29:42 -0300

    Update Controller

---
+++ src/main/java/com/login_auth_api/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.login_auth_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "*")
+public class UserController {
+    @GetMapping
+    public ResponseEntity<String> getUser() {
+        return ResponseEntity.ok("Success");
+    }
+}
